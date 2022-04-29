d468be0a4a3f1d77b94e9253c1b745898e2c5fc9
Evgeny Davydov <evg.a.davydov@gmail.com>
2022-04-29 09:07:43 +0300

    fix 02-01-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -2,7 +2,7 @@ function sumSalary(salaries) {
   let result = 0;
 
   for (let key in salaries) {
-    if ( isNaN(salaries[key]) || !(isFinite(salaries[key])) ) break;
+    if ( !(isFinite(salaries[key])) ) break;
     result += salaries[key];
   }
 
