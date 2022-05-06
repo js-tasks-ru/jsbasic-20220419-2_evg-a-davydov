bfd896a0e17c58f6dd72ec8787197ecb45162df4
Evgeny Davydov <evg.a.davydov@gmail.com>
2022-05-06 15:50:13 +0300

    add 03-05-task solution

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
   // ваш код...
+  let sortedNum = str.split(' ')
+               .filter( item => isFinite(item) )
+               .sort( (a, b) => a - b );
+
+  return {
+    min: +sortedNum[0],
+    max: +sortedNum[sortedNum.length - 1],
+  }
+  
 }
