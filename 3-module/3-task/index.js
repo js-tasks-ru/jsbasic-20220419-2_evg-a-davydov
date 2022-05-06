5f53a514c217e5a1637d98a5af2961b01eafa5e8
Evgeny Davydov <evg.a.davydov@gmail.com>
2022-05-06 13:10:31 +0300

    add 03-03-task solution

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,6 @@
 function camelize(str) {
   // ваш код...
+  return str.split('-')
+            .reduce( (result, item) => 
+                        result + "" + item[0].toUpperCase() + item.slice(1) );
 }
