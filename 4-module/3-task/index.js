17b25701e381d04f7b939feb3a85177850d3d279
Evgeny Davydov <evg.a.davydov@gmail.com>
2022-05-11 16:45:05 +0300

    add 04-03-task solution

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
   // ваш код...
+  let tr = table.rows;
+
+  for (let i = 1; i < tr.length; i++) {
+    let age = tr[i].cells[1];
+    let gender = tr[i].cells[2];
+    let status = tr[i].cells[3];
+
+    if (status.getAttribute('data-available') == 'true') {
+      tr[i].classList.add('available');
+    } else {
+      tr[i].classList.add('unavailable');
+    }
+
+    tr[i].hidden = !(status.hasAttribute('data-available'));
+
+    if (gender.textContent == 'm') {
+      tr[i].classList.add('male');
+    } else {
+      tr[i].classList.add('female');
+    }
+
+    if (+age.textContent < 18) {
+      tr[i].style.textDecoration = 'line-through';
+    }
+  } 
 }
