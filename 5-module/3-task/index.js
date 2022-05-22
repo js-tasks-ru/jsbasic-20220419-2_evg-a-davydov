96673a0a5fea53cad89f3f826f8e158e8e096e69
Evgeny Davydov <evg.a.davydov@gmail.com>
2022-05-22 21:33:39 +0300

    add 05-03-task solution

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,21 @@
 function initCarousel() {
   // ваш код...
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const inner = document.querySelector('.carousel__inner');
+  let positionX = 0;
+
+  arrowLeft.style.display = 'none';
+
+  arrowRight.addEventListener('click', () => {
+      inner.style.transform = `translateX(${positionX -= inner.offsetWidth}px)`;
+      arrowLeft.style.display = (positionX === 0) ? 'none' : '';
+      arrowRight.style.display = (positionX === -inner.offsetWidth * 3) ? 'none' : '';
+   });
+
+  arrowLeft.addEventListener('click', () => {
+      inner.style.transform = `translateX(${positionX += inner.offsetWidth}px)`;
+      arrowLeft.style.display = (positionX === 0) ? 'none' : '';
+      arrowRight.style.display = (positionX === -inner.offsetWidth * 3) ? 'none' : '';
+   });
 }
