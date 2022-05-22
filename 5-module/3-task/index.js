7151183b45520139ff876fbac3d0f9b408a6e7c5
Evgeny Davydov <evg.a.davydov@gmail.com>
2022-05-22 21:44:22 +0300

    fix 05-03-task solution

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,5 +1,6 @@
 function initCarousel() {
   // ваш код...
+  const carousel = document.querySelector('.carousel');
   const arrowRight = document.querySelector('.carousel__arrow_right');
   const arrowLeft = document.querySelector('.carousel__arrow_left');
   const inner = document.querySelector('.carousel__inner');
@@ -7,15 +8,12 @@ function initCarousel() {
 
   arrowLeft.style.display = 'none';
 
-  arrowRight.addEventListener('click', () => {
-      inner.style.transform = `translateX(${positionX -= inner.offsetWidth}px)`;
-      arrowLeft.style.display = (positionX === 0) ? 'none' : '';
-      arrowRight.style.display = (positionX === -inner.offsetWidth * 3) ? 'none' : '';
-   });
+  carousel.addEventListener('click', () => {
+    arrowLeft.style.display = (positionX === 0) ? 'none' : '';
+    arrowRight.style.display = (positionX === -inner.offsetWidth * 3) ? 'none' : '';
+  });
 
-  arrowLeft.addEventListener('click', () => {
-      inner.style.transform = `translateX(${positionX += inner.offsetWidth}px)`;
-      arrowLeft.style.display = (positionX === 0) ? 'none' : '';
-      arrowRight.style.display = (positionX === -inner.offsetWidth * 3) ? 'none' : '';
-   });
+  arrowRight.addEventListener('click', () => inner.style.transform = `translateX(${positionX -= inner.offsetWidth}px)`);
+
+  arrowLeft.addEventListener('click', () => inner.style.transform = `translateX(${positionX += inner.offsetWidth}px)`);
 }
