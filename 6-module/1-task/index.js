e5e7ab86a9c2911e68839449cca1c385108dfe17
Evgeny Davydov <evg.a.davydov@gmail.com>
2022-05-26 20:16:23 +0300

    add 06-01-task solution

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,45 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = this.createTable();
+    this.addTableElements(this.elem, rows);
+  }
+
+  createTable() {
+    const table = document.createElement('TABLE');
+
+    table.innerHTML = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+    `;
+
+    return table;
+  }
+
+  addTableElements(table, rows) {
+    table.innerHTML += rows.map(item => {
+      return `
+        <tr>
+          <td>${item.name}</td>
+          <td>${item.age}</td>
+          <td>${item.salary}</td>
+          <td>${item.city}</td>
+          <td><button>X</button></td>
+        </tr>
+      `;
+    }).join('');
+
+    table.addEventListener('click', (event) => {
+      if (event.target.tagName === 'BUTTON') {
+        event.target.closest('TR').remove();
+      }  
+    });
   }
 }
